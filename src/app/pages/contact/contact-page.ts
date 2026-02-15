c0a0bab0658edb2aec8dbe9635bc054544c3c35d
Joseph Montoya <j.jhoel@outlook.es>
2026-02-15 16:04:03 -0500

    First commit

---
+++ src/app/pages/contact/contact-page.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-page',
+  imports: [],
+  templateUrl: './contact-page.html',
+})
+export default class ContactPage { }
